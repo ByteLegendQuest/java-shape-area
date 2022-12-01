870853f0c9708bf8ceb6666328bbe20248973c92
Tomcat-Guo <zhiqiang.guo@shopee.com>
2022-12-01 02:43:27 +0000

    Update at 2022-12-01T02:43:27.480847Z

--- src/main/java/com/bytelegend/Circle.java
+++ src/main/java/com/bytelegend/Circle.java
@@ -1,16 +1,13 @@
 package com.bytelegend;
 
 public class Circle extends Shape {
-    /** `diameter` is the diameter of the circle. Hint: you can use `Math.PI`. */
+    /**
+     * `diameter` is the diameter of the circle. Hint: you can use `Math.PI`.
+     */
     private final double diameter;
 
     public Circle(double diameter) {
         this.diameter = diameter;
-    }
-
-    @Override
-    public double getArea() {
-        return Math.PI * (diameter/2) * (diameter/2);
     }
 
     @Override
--- src/main/java/com/bytelegend/Square.java
+++ src/main/java/com/bytelegend/Square.java
@@ -1,7 +1,9 @@
 package com.bytelegend;
 
 public class Square extends Shape {
-    /** `sideLength` is the length of the side of the square. */
+    /**
+     * `sideLength` is the length of the side of the square.
+     */
     private final double sideLength;
 
     public Square(double sideLength) {
